504543c27cd7eb1b4eadcf409931e77c85fb7c1c
Johan Yadir Huayta Enriquez <johan.huayta@utec.edu.pe>
2026-08-26 10:17:27 -0500

    Se actualizo Tensor.h

--- Tensor.h
+++ Tensor.h
@@ -1,1 +1,24 @@
+#ifndef TENSOR_H
+#define TENSOR_H
+#include <vector>
+#include <cstddef>
 
+using namespace std;
+
+class Tensor {
+private:
+  vector<size_t> shape;
+  size_t total_size;
+  double* data;
+
+public:
+  Tensor(const vector<size_t>& shape_ , const vector<double>& values_);
+  ~Tensor();
+
+  static Tensor zeros(const vector<size_t>& shape);
+  static Tensor ones(const vector<size_t>& shape);
+  static Tensor random(const vector<size_t>& shape, double min, double max);
+  static Tensor arange(double start, double end);
+};
+
+#endif // TENSOR_H
