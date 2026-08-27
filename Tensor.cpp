04ab754b0306af8ca493c0a9084275eb44f5622d
Johan Yadir Huayta Enriquez <johan.huayta@utec.edu.pe>
2026-08-26 20:58:45 -0500

    Update Tensor.cpp

--- Tensor.cpp
+++ Tensor.cpp
@@ -34,4 +34,45 @@ Tensor Tensor::zeros(const vector<size_t>& shape){
   return Tensor(shape,ceros);
 }
 
+Tensor Tensor::ones(const vector<size_t>& shape){
+  size_t total = 1;
+  for (size_t i=0; i<shape.size(); i++){
+    total *= shape[i];
+  }
+
+  vector<double> unos;
+  for(size_t i = 0; i<total; i++){
+    unos.push_back(1.0);
+  }
+  return Tensor(shape,unos);
+}
+
+Tensor Tensor::random(const vector<size_t>& shape, double min, double max){
+  size_t total = 1;
+  for (size_t i=0; i<shape.size(); i++) {
+    total *= shape[i];
+  }
+
+  vector <double> randoms;
+  for(size_t i = 0; i<total; i++){
+    double num_random = min + ((double)rand() / RAND_MAX) * (max-min);
+    randoms.push_back(num_random);
+  }
+  return Tensor(shape, randoms);
+}
+
+Tensor Tensor::arange(double start, double end) {
+  vector <double> valores;
+  for(double i = start; i < end; i++){
+    valores.push_back(i);
+  }
+
+  vector<size_t> shape_1d = {valores.size()};
+  return Tensor(shape_1d, valores);
+}
+
+
+
+
+
 
