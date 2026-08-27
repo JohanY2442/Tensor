f7c917598b7ec7455ba03b0cf3cf3ac5e2135323
Johan Yadir Huayta Enriquez <johan.huayta@utec.edu.pe>
2026-08-26 22:34:41 -0500

    Implement Tensor class memory management and operators
    Added copy constructor, move constructor, and assignment operator for Tensor class. Implemented operator overloads for addition, subtraction, and multiplication, along with view and unsqueeze methods.

--- Tensor.cpp
+++ Tensor.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// SECCION 3: Constructor y creacion de tensores pre-definidos
+
 Tensor::Tensor(const vector<size_t>& shape_ , const vector<double>& values_){
   shape = shape_;
   
@@ -70,6 +72,140 @@ Tensor Tensor::arange(double start, double end) {
   vector<size_t> shape_1d = {valores.size()};
   return Tensor(shape_1d, valores);
 }
+
+
+// SECCION 4: Gestion de memoria y ciclo de vida
+
+Tensor::Tensor(const Tensor& other){
+  shape = other.shape;
+  total_size = other.total_size;
+  data = new double[total_size];
+
+  for(size_t i = 0; i<total_size; i++){
+    data[i] = other.data[i];
+  }
+}
+
+Tensor::Tensor(Tensor&& other) noexcept {
+  shape = other.shape;
+  total_size = other.total_size;
+  data = other.data;
+  other.data = nullptr;
+  other.total_size = 0;
+  other.shape.clear();
+}
+
+Tensor& Tensor::operator=(const Tensor& other) {
+  if (this != &other) {
+    delete[] data;
+    shape = other.shape;
+    total_size = other.total_size;
+    data = new double[total_size];
+
+    for(size_t i = 0; i<total_size; i++){
+      data[i] = other.data[i];
+    }
+  }
+  return *this;
+}
+
+
+// SECCION 5: Sobrecarga de operadores
+
+Tensor Tensor::operator+(const Tensor& other) const { 
+  if (this->shape != other.shape) { 
+    throw invalid_argument("Dimensiones incompatibles para la suma."); 
+  } 
+  
+  vector<double> nuevos_valores(total_size); 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    nuevos_valores[i] = this->data[i] + other.data[i]; 
+  } 
+  return Tensor(this->shape, nuevos_valores); 
+}
+
+Tensor Tensor::operator-(const Tensor& other) const { 
+  if (this->shape != other.shape) { 
+    throw invalid_argument("Dimensiones incompatibles para la resta."); 
+  } 
+  
+  vector<double> nuevos_valores(total_size); 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    nuevos_valores[i] = this->data[i] - other.data[i]; 
+  } 
+  
+  return Tensor(this->shape, nuevos_valores); 
+} 
+
+Tensor Tensor::operator*(const Tensor& other) const { 
+  if (this->shape != other.shape) { 
+    throw invalid_argument("Dimensiones incompatibles para la multiplicacion."); 
+  } 
+  
+  vector<double> nuevos_valores(total_size); 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    nuevos_valores[i] = this->data[i] * other.data[i]; 
+  } 
+  return Tensor(this->shape, nuevos_valores); 
+} 
+
+Tensor Tensor::operator*(double scalar) const { 
+  vector<double> nuevos_valores(total_size); 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    nuevos_valores[i] = this->data[i] * scalar; 
+  } 
+  return Tensor(this->shape, nuevos_valores); 
+} 
+
+// SECCIÓN 6: MODIFICACION DE DIMENSIONES
+
+Tensor Tensor::view(const vector<size_t>& new_shape) const { 
+  if (new_shape.size() > 3) { 
+    throw invalid_argument("El numero de dimensiones no puede exceder tres."); 
+  } 
+  
+  size_t new_size = 1; 
+  
+  for (size_t i = 0; i < new_shape.size(); i++) { 
+    new_size *= new_shape[i]; 
+  } 
+  
+  if (new_size != this->total_size) { 
+    throw invalid_argument("El numero total de elementos debe mantenerse constante."); 
+  } 
+  
+  vector<double> valores; 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    valores.push_back(data[i]); 
+  } 
+  return Tensor(new_shape, valores); 
+} 
+
+Tensor Tensor::unsqueeze(size_t dim) const { 
+  if (this->shape.size() >= 3) { 
+    throw invalid_argument("El tensor ya tiene 3 dimensiones."); 
+  } 
+  
+  if (dim > this->shape.size()) { 
+    throw invalid_argument("Posicion de dimension invalida para unsqueeze."); 
+  } 
+  
+  vector<size_t> new_shape = this->shape; 
+  new_shape.insert(new_shape.begin() + dim, 1); 
+  vector<double> valores; 
+  
+  for (size_t i = 0; i < total_size; i++) { 
+    valores.push_back(data[i]); 
+  } 
+  return Tensor(new_shape, valores); 
+}
+
+
 
 
 
