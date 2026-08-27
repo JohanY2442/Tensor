0c84f0e9e0136ac744418bb4576b3351493054d3
Johan Yadir Huayta Enriquez <johan.huayta@utec.edu.pe>
2026-08-26 22:19:42 -0500

    Enhance Tensor class with constructors and operators
    Added constructors, assignment operators, and operator overloads for Tensor class.

--- Tensor.h
+++ Tensor.h
@@ -2,6 +2,7 @@
 #define TENSOR_H
 #include <vector>
 #include <cstddef>
+#include <stdexcept>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ private:
   double* data;
 
 public:
+  // SECCION 3: Constructor principal y creacion de tensores predefinidos
   Tensor(const vector<size_t>& shape_ , const vector<double>& values_);
   ~Tensor();
 
@@ -19,6 +21,26 @@ public:
   static Tensor ones(const vector<size_t>& shape);
   static Tensor random(const vector<size_t>& shape, double min, double max);
   static Tensor arange(double start, double end);
+
+
+  // SECCION 4: Gestion de memoria y ciclo de vida
+  Tensor(const Tensor& other);
+  Tensor(Tensor&& other) noexcept;
+  Tensor& operator=(const Tensor& other);
+  Tensor& operator=(Tensor&& other) noexcept;
+
+
+  // SECCION 5 : Sobrecarga de operadores
+  Tensor operator+(const Tensor& other) const;
+  Tensor operator-(const Tensor& other) const;
+  Tensor operator*(const Tensor& other) const;
+  Tensor operator*(double scalar) const;
+
+
+  // SECCION 6: Modificacion de dimensiones
+  Tensor view(const vector<size_t>& new_shape) const;
+  Tensor unsqueeze(size_t dim) const;
+
 };
 
 #endif // TENSOR_H
